ed9e08a68f7cc1ce55b6289729f336b917e1e277
Ivan Kuznetsov <strong.survives13@gmail.com>
2026-06-20 17:35:07 +0300

    feat(gl-sbx-4): Camera and coordinates task implemented

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -4,8 +4,6 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
-// in vec3 ourPosition;
 in vec2 texCoord;
 
 uniform sampler2D texture1;
@@ -14,7 +12,6 @@ uniform float mixFactor;
 
 void main()
 {
-    //FragColor = vec4(ourColor, 1.0);
     FragColor = mix(texture(texture1, texCoord),
                     texture(texture2, texCoord),
                     mixFactor);
